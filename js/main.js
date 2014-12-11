4370de2a43d1d9774be18c58c88a53b54d1bbef0
Brian Clifton <briancliftonstudio@gmail.com>
2014-12-11 17:34:26 -0500

    Updated article text.

--- js/main.js
+++ js/main.js
@@ -316,25 +316,15 @@ function loadData(data, timeline, gdelt, target, name) {
 }
 
 function drawGraph(data, timeline, gdelt, target, title) {
+
+	// console.log(data);
 	// console.log(gdelt);
-	// console.log('data', data);
-
-	console.log(data);
-	console.log(gdelt);
-
-
-	d3.select('#graphics')
-		.append('div')
-		.attr('id', target)
-		// .append('h3')
-		// .text(title)
-		;
 
 	var tip = d3.tip().attr('class', 'd3-tip').html(function(d) { return d.headline; });
 
-	var margin = {top: 20, right: 20, bottom: 30, left: 40};
+	var margin = {top: 20, right: 60, bottom: 30, left: 40};
 	var width = 1000 - margin.left - margin.right;
-	var height = 300 - margin.top - margin.bottom;
+	var height = 350 - margin.top - margin.bottom;
 
 	var x = d3.time.scale()
 		.domain([
@@ -348,8 +338,7 @@ function drawGraph(data, timeline, gdelt, target, title) {
 	var y = d3.scale.linear()
 		.domain([
 			0,
-			30
-			// d3.max(data, function(d) { return d.i; })
+			35
 		])
 		.range([height, 0]);
 
@@ -364,7 +353,7 @@ function drawGraph(data, timeline, gdelt, target, title) {
 	    .scale(y)
 	    .orient("left");
 
-	var svg = d3.select('#'+target).append("svg")
+	var svg = d3.select('#graphic').append("svg")
 	    .attr("width", width + margin.left + margin.right)
 	    .attr("height", height + margin.top + margin.bottom)
 		.append("g")
@@ -388,62 +377,16 @@ function drawGraph(data, timeline, gdelt, target, title) {
 
 	svg.call(tip);
 
-	//////////////////////////////////////////////////////
-	// Vertical event lines
-	var linegroup = svg.append('g');
-	var line = d3.svg.line()
-		.x(function(d) { return x(new Date(d)); })
-		.y(function(d) { return 0; });
-	var eventDate = linegroup.selectAll('.event')
-		.data(timeline)
-		.enter();
-	// eventDate.append('line')
-	// 	.attr('x1', function(d){ return x(new Date(d.date)); })
-	// 	.attr('y1', 0)
-	// 	.attr('x2', function(d){ return x(new Date(d.date)); })
-	// 	.attr('y2', height)
-	// 	.style('stroke', 'rgba(255,0,0,0.4)')
-	// 	.style('fill', 'none');
-	eventDate.append('text')
-		.attr('class', 'event-label')
-		.attr('transform', 'rotate(-90)')
-		.attr('x', 0)
-		.attr('y', function(d) {
-			return x(new Date(d.date));
-		})
-		.attr('dy', '1em')
-		.text(function(d) {
-			return d.label;
-		});
-	//////////////////////////////////////////////////////
-
-
 
 	svg.selectAll('rect')
 		.data(data)
 		.enter()
 		.append('rect')
 		.attr('class', 'box')
-		// .attr('class', function(d) {
-
-		// 	// console.log(d.source);
-		// 	if (d.source === 'AP') {
-		// 		return 'ap'; //'rgba(239, 59, 53, 0.2)'
-		// 	} else if (d.source === 'Reuters') {
-		// 		return 'reuters'; //'rgba(252, 128, 36, 0.2)'
-		// 	} else if (d.source === 'Guardian') {
-		// 		return 'guardian'; //'rgba(51, 105, 217, 0.2)'
-		// 	} else if (d.source === 'International Herald Tribune') {
-		// 		return 'iht';
-		// 	} else {
-		// 		return 'nytimes'; //'rgba(0, 0, 0, 0.07)'
-		// 	}
-		// })
 		.attr('x', function(d){ return x(d.date); })
 		.attr('y', function(d){ return y(d.article_total); })
 		// .attr('y', function(d){ return y(d.country_article_total); })
 		.attr('width', 2)
-		// .attr('height', (height / d3.max(data, function(d) { return d.i; })) - 1 )
 		.attr('height', 8)
 		.style('fill', function(d) { 
 			if (d.article_total > 10) {
@@ -458,19 +401,39 @@ function drawGraph(data, timeline, gdelt, target, title) {
 		;
 
 
-	if (gdelt) {
-		console.log(gdelt);
-		var gline = d3.svg.line()
-			.interpolate('basis')
-			.x(function(d) { return x(d.date); })
-			.y(function(d) { return y(d.smooth); })
 
-		svg.append('path')
-			.datum(gdelt)
-			.attr('class', 'gdelt')
-			.attr('d', gline);
+	var gline = d3.svg.line()
+		.interpolate('basis')
+		.x(function(d) { return x(d.date); })
+		.y(function(d) { return y(d.smooth); });
 
-	}
+	// var gdelt_group = svg.append('g')
+	// 	.attr('class', 'gdeltgroup');
+
+	// var gd = gdelt_group.selectAll('.gdelt')
+	// 	.datum(gdelt);
+
+	// gd.append('path')
+	// 	.datum(gdelt)
+	// 	.attr('class', 'gdelt')
+	// 	.attr('d', gline);
+
+
+
+	// gpath.append("text")
+	//     .datum(function(d) { 
+	//     	// console.log(d);
+	//     	return d[d.length-1]; 
+	//     })
+	//     .attr("transform", function(d) { 
+	//     	// console.log(d);
+	//     	return 'translate(800, 25)';
+	//     	// return "translate(" + x(d.date) + "," + y(d.smooth) + ")"; 
+	//     })
+	//     .attr("x", 3)
+	//     .attr("dy", ".35em")
+	//     // .style('z-index', '1000')
+	//     .text('Protests');
 
 
 	// d3.selectAll(".button--all,.button--position").on("click.position", function() {
@@ -488,248 +451,97 @@ function drawGraph(data, timeline, gdelt, target, title) {
 	// });	
 
 
-	////////////////////////////////////////////////////////////
+	var gdelt_group = svg.append('g')
+		.attr('class', 'gdeltgroup');
 
+	gdelt_group
+		.datum(gdelt)
+		.append('path')
+		.attr('class', 'gdelt')
+		.attr('d', gline);
 
-	// var margin = {top: 10, right: 10, bottom: 100, left: 40};
-	// var margin2 = {top: 430, right: 10, bottom: 20, left: 40};
-	// var width = 960 - margin.left - margin.right;
-	// var height = 500 - margin.top - margin.bottom;
-	// var height2 = 500 - margin2.top - margin2.bottom;
-
-	// var x = d3.time.scale()
-	// 	.domain([
-	// 		d3.min(data, function(d) { return d.date; }),
-	// 		d3.max(data, function(d) { return d.date; })
-	// 	])
-	// 	.range([0, width]);
-	// var x2 = d3.time.scale()
-	// 	.domain(x.domain())
-	// 	.range([0, width]);
-	// var y = d3.scale.linear()
-	// 	.domain([0, d3.max(data, function(d) { return d.i; })])
-	// 	.range([height, 0]);
-	// var y2 = d3.scale.linear()
-	// 	.domain(y.domain())
-	// 	.range([height2, 0]);
-
-	// var xAxis = d3.svg.axis().scale(x).orient('bottom');
-	// var xAxis2 = d3.svg.axis().scale(x2).orient('bottom');
-	// var yAxis = d3.svg.axis().scale(y).orient('left');
-
-	// var brush = d3.svg.brush()
-	// 	.x(x2)
-	// 	.on('brush', brushed);
-
-	// var gline = d3.svg.line()
-	// 	.x(function(d) { return x(d.date); })
-	// 	.y(function(d) { return y(d.nummentions); })
-
-	// var gArea = d3.svg.area()
-	// 	.interpolate('monotone')
-	// 	.x(function(d) { return x(d.date); })
-	// 	.y0(height)
-	// 	.y1(function(d) { return y(d.nummentions); });
-
-	// var gArea2 = d3.svg.area()
-	// 	.interpolate('monotone')
-	// 	.x(function(d) { return x2(d.date); })
-	// 	.y0(height2)
-	// 	.y1(function(d) { return y2(d.nummentions); });
-
-	// var svg = d3.select("#graphic").append("svg")
-	//     .attr("width", width + margin.left + margin.right)
-	//     .attr("height", height + margin.top + margin.bottom)
-	// 	.append("g")
-	//     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
- //    svg.append('defs').append('clipPath')
- //    	.attr('id', 'clip')
- //    	.attr('width', width - 40)
- //    	.attr('height', height);
-
-	// var focus = svg.append("g")
-	//     .attr("class", "focus")
-	//     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-	// var context = svg.append("g")
-	//     .attr("class", "context")
-	//     .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
-
-	// // focus.append('path')
-	// // 	.datum(gdelt)
-	// // 	.attr('class', 'area')
-	// // 	.attr('d', gArea)
-	// // 	.style('stroke', 'rgba(140,173,241,0.1)')
-	// // 	.style('fill', 'rgba(140,173,241,0.3)');
-
-
-	// focus.selectAll('rect')
-	// 	.data(data)
-	// 	.enter()
-	// 	.append('rect')
-	// 	.attr('class', 'box')
-	// 	.attr('class', function(d) {
-
-	// 		// console.log(d.source);
-	// 		if (d.source === 'AP') {
-	// 			return 'ap'; //'rgba(239, 59, 53, 0.2)'
-	// 		} else if (d.source === 'Reuters') {
-	// 			return 'reuters'; //'rgba(252, 128, 36, 0.2)'
-	// 		} else if (d.source === 'Guardian') {
-	// 			return 'guardian'; //'rgba(51, 105, 217, 0.2)'
-	// 		} else {
-	// 			return 'nytimes'; //'rgba(0, 0, 0, 0.07)'
-	// 		}
-	// 	})
-	// 	.attr('x', function(d){ return x(d.date); })
-	// 	.attr('y', function(d){ return y(d.i); })
-	// 	.attr('width', 2)
-	// 	.attr('height', 10)
-	// 	// .on('mouseover', tip.show)
-	// 	// .on('mouseout', tip.hide)
-	// 	;
-
-	// focus.append('g')
-	//     .attr('class', 'x axis')
-	//     .attr('transform', 'translate(0,' + height + ')')
-	//     .call(xAxis);
-
-	// focus.append('g')
-	//     .attr('class', 'y axis')
-	//     .call(yAxis)
-	//     .append('text')
-	//     .attr('class', 'label')
-	//     .attr('transform', 'rotate(-90)')
-	//     .attr('y', 6)
-	//     .attr('dy', '0.71em')
-	//     .style('text-anchor', 'end')
-	//     .text('Number of News Articles per Day');
-
-	// context.append('path')
+	// gdelt_group
 	// 	.datum(gdelt)
-	// 	.attr('class', 'area')
-	// 	.attr('d', gArea2)
-	// 	.style('stroke', 'rgba(140,173,241,0.1)')
-	// 	.style('fill', 'rgba(140,173,241,0.3)');
+	// 	.append('text')
 
-	// context.append('g')
-	//     .attr('class', 'x axis')
-	//     .attr('transform', 'translate(0,' + height2 + ')')
-	//     .call(xAxis2);
+	gdelt_group
+	    .datum(function(d) { 
+	    	return d[d.length-1]; 
+	    })
+	    .append("text")
+	    .attr('class', 'gdelt_label')
+	    .attr("transform", function(d) { 
+	    	return "translate(" + x(d.date) + "," + y(d.smooth) + ")"; 
+	    })
+	    .attr("x", 8)
+	    .attr("dy", ".35em")
+	    .text('Protests');
 
-	// context.append('g')
-	// 	.attr('class', 'x brush')
-	// 	.call(brush)
-	// 	.selectAll('rect')
-	// 	.attr('y', -6)
-	// 	.attr('height', height2 + 7);
+	//////////////////////////////////////////////////////
+	// Vertical event lines
+	
+	var line = d3.svg.line()
+		.x(function(d) { return x(new Date(d)); })
+		.y(function(d) { return 0; });
+	
+	var linegroup = svg.append('g');
+	var eventDate = linegroup.selectAll('.event')
+		.data(timeline)
+		.enter();
 
-	// function brushed() {
-	// 	x.domain(brush.empty() ? x2.domain() : brush.extent());
-	// 	// focus.select(".area").attr("d", gArea);
-	// 	focus.select(".box").attr("width", 2);
-	// 	focus.select(".x.axis").call(xAxis);
-	// }
+	eventDate.append('line')
+		.attr('x1', function(d){ return x(new Date(d.date)); })
+		.attr('y1', 0)
+		.attr('x2', function(d){ return x(new Date(d.date)); })
+		.attr('y2', height)
+		.style('stroke', 'rgba(0,0,0,0.1)')
+		.style('fill', 'none');
+
+	eventDate.append('text')
+		.attr('class', 'event-label')
+		.attr('transform', 'rotate(-90)')
+		.attr('x', 0)
+		.attr('y', function(d) {
+			return x(new Date(d.date));
+		})
+		.attr('dy', '1em')
+		.text(function(d) {
+			return d.label;
+		});
+	//////////////////////////////////////////////////////
+
+
+	var legend = svg.append("g")
+		.attr("class", "legend")
+		.attr("x", w - 65)
+		.attr("y", 25)
+		.attr("height", 100)
+		.attr("width", 100);
+
+	legend.selectAll('rect')
+		.data(dataset)
+		.enter()
+		.append("rect")
+		.attr("x", w - 65)
+		.attr("y", 25)
+		.attr("width", 10)
+		.attr("height", 10)
+		.style("fill", function(d) { 
+			return color_hash[dataset.indexOf(d)][1]
+		});
+
+	legend.selectAll('text')
+		.data(dataset)
+		.enter()
+		.append("text")
+		.attr("x", w - 65)
+		.attr("y", 25)
+		.text(function(d) { 
+			return color_hash[dataset.indexOf(d)][0] + ": " + d;
+		});
 
 	
 } // ends drawgraph()
-
-
-function drawLineGraph(data, timeline, target) {
-
-	// console.log(data);
-
-	// var categories = [];
-	// _.each(data, function(entry) {
-	// 	var temp = {
-	// 		name: entry.headline,
-	// 		values: {
-	// 			date: entry.date,
-	// 			number: entry.i
-	// 		}
-	// 	};
-	// 	categories.push(temp);
-	// });
-	// console.log('categories', categories);
-
-	// var tip = d3.tip().attr('class', 'd3-tip').html(function(d) { return d.headline; });
-
-	var margin = {top: 20, right: 20, bottom: 30, left: 40};
-	var width = 1000 - margin.left - margin.right;
-	var height = 400 - margin.top - margin.bottom;
-
-	var x = d3.time.scale()
-		.domain([
-			new Date('1997-04-29'),
-			new Date('2014-12-02')
-			// d3.min(data, function(d) { return d.date; }),
-			// d3.max(data, function(d) { return d.date; })
-		])
-		.range([0, width]);
-
-	var y = d3.scale.linear()
-		.domain([
-			0,
-			25
-			// d3.max(data, function(d) { return d.i; })
-		])
-		.range([height, 0]);
-
-	var xAxis = d3.svg.axis()
-	    .scale(x)
-	    .orient("bottom")
-	    .ticks(20);
-
-	var yAxis = d3.svg.axis()
-	    .scale(y)
-	    .orient("left");
-
-	var svg = d3.select(target).append("svg")
-	    .attr("width", width + margin.left + margin.right)
-	    .attr("height", height + margin.top + margin.bottom)
-		.append("g")
-	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-	svg.append('g')
-	    .attr('class', 'x axis')
-	    .attr('transform', 'translate(0,' + height + ')')
-	    .call(xAxis);
-
-	svg.append('g')
-	    .attr('class', 'y axis')
-	    .call(yAxis)
-	    .append('text')
-	    .attr('class', 'label')
-	    .attr('transform', 'rotate(-90)')
-	    .attr('y', 6)
-	    .attr('dy', '0.71em')
-	    .style('text-anchor', 'end')
-	    .text('Number of News Articles per Day');
-
-	// svg.call(tip);
-
-	var line = d3.svg.line()
-	    .interpolate("basis")
-	    .x(function(d) { return x(d.date); })
-	    .y(function(d) { return y(d.count); });
-
-	
-	var countryLines = svg.selectAll('.country')
-		.data(data)
-		.enter()
-		.append('g')
-		.attr('class', 'country');
-
-	// console.log(countryLines);
-
-	countryLines.append('path')
-		.attr('class', 'line')
-		.attr('d', function(d) { return line(d.values); })
-		.attr('stroke', 'steelblue');
-
-}
-
 
 
 $(document).ready(function() {
